85687bbb667373f825dd7953df650ae74f5ce586
minhtranz <s3978609@rmit.edu.vn>
2023-05-11 09:17:40 +0700

    ss

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // session
 // const session = require('express-session');
@@ -26,6 +27,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //ejs use
+// set views folder
+app.set("views", path.join(__dirname, "src", "views"));
 app.set('view engine', 'ejs');
 const port = 3000;
 
--- src/routes/signup.js
+++ src/routes/signup.js
@@ -21,7 +21,16 @@ const User = require('../model/user');
 const passwordregex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,20}$/;
 
 // conf localstrategy
-passport.use(new LocalStrategy(function(username, password, done) {
+passport.use(new LocalStrategy({passReqToCallback: true
+},function(req, username, password, done) {
+    // input
+    const role = req.body.role;
+    // TODO: Find a way to import picture -> mongoose
+    const name = req.body.name;
+    const customer_address = req.body.customer_address;
+    const businessname = req.body.businessname;
+    const businessaddress = req.body.businessaddress;
+    const distributionHub = req.body.distributionHub;
     // find the user
     User.findOne({ username: username})
     .then((user) => {
@@ -29,19 +38,28 @@ passport.use(new LocalStrategy(function(username, password, done) {
         if (user) {
         return done(null, false, { message: "Username already taken." });
         }
+
+        // define what data to sent
         const newUser = new User({
+        name: name,
         username: username,
+        role: role,
+        customer_address: customer_address,
+        businessname: businessname,
+        businessaddress: businessaddress,
+        distributionHub: distributionHub, 
         encrypted_password: bcrypt.hashSync(password, 10),
         });
-        newUser.save();
+        newUser.save(); 
         return done(null, newUser);
-    })
-    .catch((err) => {
+    }).catch((err) => {
+        console.log(err)    
         return done(err);
     });
-}
-));
+}));
 
+
+// TODO Possibly optimize the code -> middleware
 // validate & upload data -> database
 router.post('/', 
     [check('password', 'Password must include one uppercase character, one lowercase character, a number, and a special character.').matches(passwordregex)],
