3464d9d3cacd702dd7bf116a6c1ae5ec4ed6e6c2
minhtranz <s3978609@rmit.edu.vn>
2023-05-15 17:22:18 +0700

    fix model

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,37 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-const Product = require('../model/product');
-
+const Product = require("../model/product");
 
 //@GET /product/:id
 //@desc render detail page
 //@access public
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    Product.findById(id)
-        .then((matchedProduct) => {
-            if (matchedProduct) {
-                res.render('detail', { product: matchedProduct });
-            } else {
-                res.render('error', { message: 'Product not found' });
-            }
-        })
-        .catch((error) => {
-            console.log('Error retrieving product:', error);
-            res.render('error', { message: 'Error retrieving product' });
-        });
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Product.findById(id)
+    .then((matchedProduct) => {
+      if (matchedProduct) {
+        res.render("detail", { product: matchedProduct });
+      } else {
+        res.render("error", { message: "Product not found" });
+      }
+    })
+    .catch((error) => {
+      console.log("Error retrieving product:", error);
+      res.render("error", { message: "Error retrieving product" });
+    });
 });
 
 // @route GET /product
 // @desc render all products
 // @access public
-router.get('/', (req, res) => {
-    Product.find()
-        .then((products) => {
-            res.render('product', { products: products });
-        })
-        .catch((error) => console.log(error.message));
+router.get("/", (req, res) => {
+  Product.find()
+    .then((products) => {
+      res.render("product", { products: products });
+    })
+    .catch((error) => console.log(error.message));
 });
 
 module.exports = router;
--- src/routes/vendor.js
+++ src/routes/vendor.js
@@ -1,38 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
+const Product = require("../model/product");
 // VENDOR ROUTES
 
 //@route GET /vendor/addproduct
 //@desc render add product page
 //@access private
-router.get('/addproduct', (req, res) => {
-    res.render('add_product');
+router.get("/addproduct", (req, res) => {
+  res.render("add_product");
 });
 
 // @route GET /vendor/viewproduct
 // @desc render all product from a vendor
 // @access private
-router.get('/viewproduct', (req, res) => {
-    Product.find()
+router.get("/viewproduct", (req, res) => {
+  Product.find()
     .then((products) => {
-        res.render('view_product', {products: products});
-    }
-)
+      res.render("view_product", { products: products });
+    })
     .catch((error) => console.log(error.message));
-    }
-);
-  
+});
+
 //@route POST /vendor/add
 //@desc add new product
 //@access public
-router.post('/addproduct', (req, res) => {
-// console.log(req.body);
-const product = new Product(req.body);
-product.save()
-    .then(() => res.redirect('/product'))
-    .catch(error => res.send(error));
+router.post("/addproduct", (req, res) => {
+  // console.log(req.body);
+  const product = new Product(req.body);
+  product
+    .save()
+    .then(() => res.redirect("/product"))
+    .catch((error) => res.send(error));
 });
 
 module.exports = router;
